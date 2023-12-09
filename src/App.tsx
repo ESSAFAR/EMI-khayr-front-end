03902f42a1e8994691c6620cf95982073a6784db
Lambda <anwar.essafar@gmail.com>
2023-12-09 21:33:21 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className='className="text-3xl font-bold underline'>EMI khayr t3ich t3ich t3ich</div>
+    </>
+  )
+}
+
+export default App
